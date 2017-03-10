611b29674df2b6114e44d5aa343111f12e3c7fb9
hugo <hfernandez@neuquen.gov.ar>
2017-03-10 12:29:35 -0300

    mejoras mpiUpdater

--- updateMpi.ts
+++ updateMpi.ts
@@ -22,105 +22,133 @@ export class UpdateMpi {
                 'estado': 'validado'
             }
             mongodb.MongoClient.connect(url, function (err, db) {
-                if (err) {
-                    console.log('Error al conectarse a Base de Datos: ', err);
-                }
-                let cursorPacientes = db.collection(coleccion).find(condicion).stream();
-                cursorPacientes.on('data', function (data) {
-                    console.log('paciente: ', data);
-                    if (data != null) {
-                        /*Hacemos una pausa para que de tiempo a la inserción y luego al borrado del paciente*/
-                        cursorPacientes.pause();
-                        console.log('despues de la pausa');
-                        existeEnMpi(data, coleccion)
-                            .then((resultado => {
-                                /*Si NO hubo matching al 100% lo tengo que insertar en MPI */
-                                console.log('volvio de existeEnMpi', resultado);
-                                if (resultado !== {}) {
-                                    pacientesInsertados.push(data);
-                                    post.cargarUnPacienteMpi(data)
-                                        .then((rta) => {
-                                            console.log('Paciente Guardado es:', data);
-                                        });
-                                };
-                                /*Borramos el paciente de ANDES*/
-                                db.collection(coleccion).remove({
-                                        '_id': data._id
-                                    },
-                                    function (err, item) {
-                                        if (err) {
-                                            console.log('Error al querer borrar el paciente ', err);
-                                        } else {
-                                            console.log('Paciente borrado: ', data);
-                                        }
-                                    });
-                                /*Restablecemos el flujo de trabajo*/
-                                cursorPacientes.resume();
-                            }))
+                    if (err) {
+                        console.log('Error al conectarse a Base de Datos: ', err);
                     }
-                });
-
+                    let cursorPacientes = db.collection(coleccion).find(condicion).stream();
+                    cursorPacientes.on('data', function (data) {
+                            console.log('paciente: ', data);
+                            if (data != null) {
+                                /*Hacemos una pausa para que de tiempo a la inserción y luego al borrado del paciente*/
+                                cursorPacientes.pause();
+                                console.log('despues de la pausa');
+                                existeEnMpi(data, coleccion)
+                                    .then((resultado => {
+                                            /*Si NO hubo matching al 100% lo tengo que insertar en MPI */
+                                            if (resultado[0] !== 'merge') {
+                                                console.log('entro');
+                                                pacientesInsertados.push(resultado[1]);
+                                                post.cargarUnPacienteMpi(resultado[1])
+                                                    .then((rta) => {
+                                                        console.log('Paciente Guardado es:', resultado[1]);
+                                                    });
+                                            } else {
+                                                /*Se fusionan los pacientes*/
+                                                console.log('entro por merge se va a fusionar: ', resultado[1]);
+                                                let pacFusionar = resultado[1];
+                                                let idPacMpi = resultado[1]._id;
+                                                console.log('id del paciente original a updetear: ', idPacMpi);
+                                                db.collection(coleccion).update(
+                                                    {'_id': idPacMpi},
+                                                    {
+                                                       $addToSet: {
+                                                            'identificadores': {
+                                                                $each: pacFusionar.identificadores
+                                                            },
+                                                            'direccion': {
+                                                                $each: pacFusionar.direccion
+                                                            },
+                                                               'contacto': {
+                                                                $each: pacFusionar.contacto
+                                                            },
+                                                               'relaciones': {
+                                                                $each: pacFusionar.relaciones
+                                                            }
+                                                        },
+                                                    },
+                                                {upsert: true},
+                                                function (err) {
+                                                    if (err) {
+                                                        console.log('Error update', err);
+                                                    }
+                                                });
+                                            }
+                                        /*Borramos el paciente de ANDES*/
+                                        db.collection(coleccion).remove({
+                                                '_id': data._id
+                                            },
+                                            function (err, item) {
+                                                if (err) {
+                                                    console.log('Error al querer borrar el paciente ', err);
+                                                } else {
+                                                    console.log('Paciente borrado: ', data);
+                                                }
+                                            });
+                                        /*Restablecemos el flujo de trabajo*/
+                                        cursorPacientes.resume();
+                                    }))
+                        };
+                    });
                 /*Finaliza el recorrido del cursor con los datos de pacientes validados */
                 cursorPacientes.on('end', function () {
                     console.log('El proceso de actualización ha finalizado, total de pacientes insertados en MPI: ', pacientesInsertados.length);
                     return true;
                 });
-
             });
-        } catch (err) {
-            console.log('Entra por este error de catch:', err);
-            return false;
+    } catch (err) {
+        console.log('Entra por este error de catch:', err);
+        return false;
+    };
+
+    /*Verfica que el paciente que si desea insertar en MPI no exista previamente*/
+    function existeEnMpi(pacienteBuscado, coleccionPaciente) {
+        let url = config.urlMongoMpi;
+        let pacientesEnMpi: any = [];
+        let match = new matching();
+        let tipoDeMatching = 'Levenshtein';
+        let porcentajeMatcheo;
+        let condicion = {
+            'claveBlocking.0': pacienteBuscado.claveBlocking[0]
         };
-
-        /*Verfica que el paciente que si desea insertar en MPI no exista previamente*/
-        function existeEnMpi(pacienteBuscado, coleccionPaciente) {
-            let url = config.urlMongoMpi;
-            let pacientesEnMpi: any = [];
-            let match = new matching();
-            let tipoDeMatching = 'Levenshtein';
-            let porcentajeMatcheo;
-            let condicion = {
-                'claveBlocking.0': pacienteBuscado.claveBlocking[0]
-            };
-            let weights = {
-                identity: 0.3,
-                name: 0.3,
-                gender: 0.1,
-                birthDate: 0.3
-            };
-            console.log('antes de llamar a la promise de control de existencia en mpi');
-            return new Promise((resolve, reject) => {
-                mongodb.MongoClient.connect(url, function (err, db) {
-                    if (err) {
-                        console.log('Error de conexión con ', err);
-                        reject(err);
-                    } else {
-                        /*Busco todos los pacientes en MPI que caen en ese bloque */
-                        console.log('entro a buscar los pacientes de bloque');
-                        pacientesEnMpi = db.collection(coleccionPaciente).find(condicion).stream();
-                        pacientesEnMpi.on('end', function () {
-                            resolve({}); 
-                            db.close();
-                        });
-                        pacientesEnMpi.on('data', function (data) {
-                            console.log('ingreso al pacientes en mpi stream', data);
-                            if (data != null) {
-                                let pacienteDeMpi = data;
-                                porcentajeMatcheo = match.matchPersonas(pacienteBuscado, pacienteDeMpi, weights, tipoDeMatching);
-                                if (porcentajeMatcheo < 1) {
-                                    db.close();
-                                    resolve({pacienteBuscado});
-                                } else {
-                                    console.log('debería devolver vacio');
-                                    /*Encontre el paciente al 100% */
-                                    resolve({});
-                                }
+        let weights = {
+            identity: 0.3,
+            name: 0.3,
+            gender: 0.1,
+            birthDate: 0.3
+        };
+        console.log('antes de llamar a la promise de control de existencia en mpi');
+        return new Promise((resolve, reject) => {
+            mongodb.MongoClient.connect(url, function (err, db) {
+                if (err) {
+                    console.log('Error de conexión con ', err);
+                    reject(err);
+                } else {
+                    /*Busco todos los pacientes en MPI que caen en ese bloque */
+                    console.log('entro a buscar los pacientes de bloque');
+                    pacientesEnMpi = db.collection(coleccionPaciente).find(condicion).stream();
+                    pacientesEnMpi.on('end', function () {
+                        resolve(['insertarNuevo', pacienteBuscado]);
+                        db.close();
+                    });
+                    pacientesEnMpi.on('data', function (data) {
+                        if (data != null) {
+                            let pacienteDeMpi = data;
+                            porcentajeMatcheo = match.matchPersonas(pacienteBuscado, pacienteDeMpi, weights, tipoDeMatching);
+                            if (porcentajeMatcheo < 1) {
+                                console.log('ingreso por menor que 1 ', porcentajeMatcheo);
+                                resolve(['insertarNuevo', pacienteBuscado]);
+                            } else {
+                                console.log('Entro por igual a 1 por lo que hay que hacer el merge del paciente', porcentajeMatcheo);
+                                /*Encontre el paciente al 100% */
+                                resolve(['merge', pacienteBuscado]);
                             }
-                        });
-                    }
-                });
+                            db.close();
+                        }
+                    });
+                }
             });
+        });
 
-        }
     }
+}
 }
